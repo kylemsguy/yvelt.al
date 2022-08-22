48a67871ec80accbd7fd568ee2f900adfd32d4e2
Kyle Zhou <kylezhou2002@hotmail.com>
2022-08-21 17:54:13 -0700

    Initial commit

---
+++ browserdetect.js
@@ -0,0 +1,52 @@
+var isMobile = {
+
+    Android: function() {
+
+        return navigator.userAgent.match(/Android/i);
+
+    },
+
+    BlackBerry: function() {
+
+        return navigator.userAgent.match(/BlackBerry/i);
+
+    },
+
+    iOS: function() {
+
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+
+    },
+
+    Opera: function() {
+
+        return navigator.userAgent.match(/Opera Mini/i);
+
+    },
+
+    Windows: function() {
+
+        return navigator.userAgent.match(/IEMobile/i);
+
+    },
+
+    any: function() {
+
+        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+
+    }
+
+};
+
+$(function() {
+	// detect if mobile
+	if(isMobile.any()){
+		console.log("Is mobile");
+		var yveltal = $('#yveltal');
+		yveltal.removeAttr("height");
+		yveltal.attr("width", "100%");
+	} else {
+		console.log("Is desktop");
+	}
+});
+
